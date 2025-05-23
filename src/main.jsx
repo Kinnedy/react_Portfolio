d099f9492922a15bdc554ec1870898833ef8fe6a
Kinnedy <taylorkinnedy@gmail.com>
2025-05-22 19:17:06 -0500

    added file extention in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import App from './App';
-import Error from './pages/Error';
-import AboutMe from './pages/AboutMe';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
+import Error from './pages/Error.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
 
 
 // Define the accessible routes, and which components respond to which URL
